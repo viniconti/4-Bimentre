8f98a7ff15d9f71e9f90ab49a335e831bf35ce97
Vinicius Conti <vinicius.sconti@eaportal.org>
2025-10-17 18:36:49 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState('');
+  const mudancadeTitulo = (e) => {
+    setTitle(e.target.value);
+  }
 
   return (
     <>
-      
+      <div className="geral">
+        <div className="container">
+
+          <div className="inputs">
+
+            <label htmlFor="">Username </label>
+              <input 
+              className="input" 
+              type="text"
+              placeholder="Username"
+              />
+              
+              <label htmlFor="">Email</label>
+              <input 
+              className="input" 
+              type="email" 
+              name="Email"
+              placeholder="Email" 
+              />
+
+          </div>
+
+          <div className="register">
+            <button className="button" onChange={(e) => mudancadeTitulo(e)}>Register</button>
+          </div>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
